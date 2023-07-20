14125294b509fa30b6ad6879747f97fae542406e
ryusuke Yamada <ryusuke.yamada.18@gmail.com>
2023-07-20 23:02:05 +0900

    Update orignal.js

--- orignal.js
+++ orignal.js
@@ -5,7 +5,7 @@ var ajaxCall = (key, url, prompt) => {
       type: "POST",
       dataType: "json",
       data: JSON.stringify({
-        model: "text-davinci-002",
+        model: "davinci:ft-sacchatgpt-2023-07-13-11-11-59",
         prompt: prompt,
         max_tokens: 1024,
         n: 1,
