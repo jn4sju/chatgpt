fe55176f9fbc0522c3bb7a7e8109ee54d2eaad21
ryusuke Yamada <ryusuke.yamada.18@gmail.com>
2023-07-20 22:39:14 +0900

    Add files via upload

---
+++ orignal.js
@@ -0,0 +1,28 @@
+var ajaxCall = (key, url, prompt) => {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: url,
+      type: "POST",
+      dataType: "json",
+      data: JSON.stringify({
+        model: "text-davinci-002",
+        prompt: prompt,
+        max_tokens: 1024,
+        n: 1,
+        temperature: 0.5,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${key}`,
+      },
+      crossDomain: true,
+      success: function (response, status, xhr) {
+        resolve({ response, status, xhr });
+      },
+      error: function (xhr, status, error) {
+        const err = new Error('xhr error');
+        err.status = xhr.status;
+        reject(err);
+      },
+    });
+  });
