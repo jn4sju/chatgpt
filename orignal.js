4ac64788f96733613dd61132c4bcb777183e5f4f
soshi-Kawata <39051117+soshi-Kawata@users.noreply.github.com>
2023-08-18 15:48:24 +0900

    Add files via upload

--- orignal.js
+++ orignal.js
@@ -5,11 +5,12 @@ var ajaxCall = (key, url, prompt) => {
       type: "POST",
       dataType: "json",
       data: JSON.stringify({
-        model: "davinci:ft-sacchatgpt-2023-07-13-11-11-59",
+        model: "davinci:ft-sacchatgpt-2023-08-13-14-07-02",
         prompt: prompt,
-        max_tokens: 1024,
+        max_tokens: 512,
         n: 1,
-        temperature: 0.5,
+		stop: None,
+        temperature: 0,
       }),
       headers: {
         "Content-Type": "application/json",
